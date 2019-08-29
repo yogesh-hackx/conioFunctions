e1998fe07eb4b3259bbc7317d6f4e10dfa118e77
yogesh-hackx <yogesh.hackx@gmail.com>
2019-08-29 17:47:46 +0530

    added library with some basic functions

---
+++ con.h
@@ -0,0 +1,38 @@
+#include <termios.h>
+#include <stdio.h>
+
+static struct termios old, neww;
+
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old);
+  neww = old;
+  neww.c_lflag &= ~ICANON;
+  neww.c_lflag &= echo ? ECHO : ~ECHO;
+  tcsetattr(0, TCSANOW, &neww);
+}
+
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+ 
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+ 
+
+char getch(void) 
+{
+  return getch_(0);
+}
+ 
+char getche(void) 
+{
+  return getch_(1);
+}
